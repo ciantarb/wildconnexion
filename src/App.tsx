55d09aebf7c0b4026ce250ae01cc4cd015b1c8d1
ciant <ciantar.benoit@gmail.com>
2022-02-01 21:45:17 +0100

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.scss';
+import { ReactElement } from 'react';
+
+
+function App() : ReactElement {
+  return (
+    <div className="App">
+      <h1>TOTO</h1>
+    </div>
+  );
+}
+
+export default App;
